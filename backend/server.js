0a12fe5a0a98880766dcecfdcb6c7927c4e1d055
sjiya114 <sjiya04072004@gmail.com>
2025-08-02 23:10:32 +0530

    edit

--- backend/server.js
+++ backend/server.js
@@ -12,14 +12,14 @@ connectCloudinary();
 app.use(cors());
 app.use(express.json()); // for application/json only
 app.use(express.urlencoded({ extended: true })); // for application/x-www-form-urlencoded
-app.use("/api/user",user);
-app.use("/api/session",session);
-app.get("/api",(req,res)=>
+app.use("/user",user);
+app.use("/session",session);
+app.get("/",(req,res)=>
 {
     res.send("app is running");
 })
-// const PORT=5000;
-// app.listen(PORT,()=>
-// {
-//     console.log(`app started running on http://localhost:${PORT}`);
-// })
+const PORT=5000;
+app.listen(PORT,()=>
+{
+    console.log(`app started running on http://localhost:${PORT}`);
+})
